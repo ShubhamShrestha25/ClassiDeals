19be907a4149e3affc066a5ab1728f1652fe71ff
Shubham <shubhamshrestha25@gmail.com>
2022-01-09 10:05:21 +0545

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import ProductDetails from './components/products/product details/ProductDetails';
-import Homepage from './pages/homepage/HomePage';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/HomePage";
+import ProductDetails from "./pages/product details/ProductDetails";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/details" element={<ProductDetails />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/details" element={<ProductDetails />} />
       </Routes>
-     
     </div>
   );
 }
